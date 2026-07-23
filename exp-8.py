db61046db398148a98fb99b8108c704ab0800ff3
shanmukh <karanamvenkatasaishanmukh@gmail.com>
2026-07-23 10:59:12 +0530

    Delete exp-8.py

--- exp-8.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df = pd.read_csv("sales.csv")
-pivot = pd.pivot_table(df,
-                       values="Units",
-                       index="Item",
-                       aggfunc="sum")
-print(pivot)
