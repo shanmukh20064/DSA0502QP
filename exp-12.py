02fc9c897db8902bc24e045c88d6a38b4f1ce9ba
shanmukh <karanamvenkatasaishanmukh@gmail.com>
2026-07-23 10:55:27 +0530

    Delete exp-12.py

--- exp-12.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-df = pd.DataFrame(np.random.randint(1,100,size=(10,4)),
-                  columns=["A","B","C","D"])
-print(df)
-df.style.set_properties(**{
-    "background-color":"black",
-    "color":"yellow"
-})
